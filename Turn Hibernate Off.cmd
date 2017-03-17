f3449d3a6e65ba87ff4cb7535b1654fa54ccbde2
Stotaku <williamqzy0705@gmail.com>
2017-03-17 11:34:50 +1100

    Hibernate on/off
    Hibernate on/off cmd

---
+++ Turn Hibernate Off.cmd
@@ -0,0 +1,8 @@
+@echo off
+Color 2f
+::Turn off the hibernate function of current Windows
+powercfg -h off
+echo Succesfully Turn off the hibernate function
+echo Press any key to exit
+pause>nul
+
---
+++ Turn Hibernate On.cmd
@@ -0,0 +1,7 @@
+@echo off
+Color 2f
+::Turn on Hibernate
+powercfg -h on
+echo Succesfully Turn on the hibernate function
+echo Press any key to exit
+pause>nul
